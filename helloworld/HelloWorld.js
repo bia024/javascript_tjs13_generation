5cb5280c3fc748a79b7a5b907d78b5a4b588d5ab
Bianca-Caetano <biancacaetano079@gmail.com>
2026-01-26 11:31:14 -0300

    Projeto Hello World

---
+++ helloworld/HelloWorld.js
@@ -0,0 +1,14 @@
+const leia = require("readline-sync");
+
+let mensagem = leia.question("Digite uma mensagem:");
+
+console.log(mensagem);
+
+
+//exemplo em aula, com base no questionamento de um aluno chamado José
+// let numero1= leia.questionInt("Digite o primeiro número: ");
+// let numero2= leia.questionInt("Digite o segundo número: ");
+
+// let soma = numero1 + numero2;
+
+// console.log("Soma: ", soma);
---
+++ helloworld/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let mensagem = leia.question("Digite uma mensagem:");
+
+// console.log(mensagem);
